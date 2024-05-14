3d4e77404d851da7dcd6ab185e337b91191bfd6f
Gompou Loua Alassane KONE <gkone@MCICARECI.LOCAL>
2024-05-14 15:28:37 +0000

    Re-deploying ...

--- model/database.py
+++ model/database.py
@@ -1,23 +1,16 @@
-import SQLAlchemy
-from SQLAlchemy import create_engine
-from SQLAlchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-
-
-# Load environment variables from .env file
 load_dotenv()
 
-# Get database URL from environment variable
-POSTGRESQL_DB_URL = os.getenv('DB_URL_STRING')
 
-# Create engine
-engine = create_engine(POSTGRESQL_DB_URL, echo=True)  # Remove echo=True for production
-DBSession = sessionmaker(bind=engine, autoflush=False)  # Include bind parameter
+POSTGRESQL_DB_URL = os.getenv('DB_URL_STRING')
+engine = create_engine(POSTGRESQL_DB_URL, echo=True)  
+DBSession = sessionmaker(bind=engine, autoflush=False)
 
 try:
-    # Test the connection
     with engine.connect() as connection:
-        print("Connected to database successfully!")
+        print("Connexion réussie!")
 except Exception as e:
-    print(f"Unable to connect to the database: {e}")
+    print(f"Echec de connexion : {e}")
